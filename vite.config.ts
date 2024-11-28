4b1d6ba0b87214b85fc563254aa2a5cbbea14d64
eapepe0 <eapepe0@gmail.com>
2024-11-27 23:18:54 -0300

    Deploy updated site

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://cristian-oyola.github.io",
+  base: "/eapepe0.github.io/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
